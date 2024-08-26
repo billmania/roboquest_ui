7c2f27144eb0e189d8c5cd71a33406d13d4d2916
Bill Mania <bill@manialabs.us>
2024-08-26 11:41:39 -0500

    Updated package.xml and src/params.js to v32.
    https://github.com/billmania/roboquest_ui/issues/146

--- src/params.js
+++ src/params.js
@@ -8,7 +8,7 @@
 const path = require('path')
 
 const RQ_PARAMS = {}
-RQ_PARAMS.VERSION = '32rc1'
+RQ_PARAMS.VERSION = '32'
 
 RQ_PARAMS.CONFIG_FORMAT_VERSION = '8'
 RQ_PARAMS.SERVER_STATIC_DIR = path.join(
