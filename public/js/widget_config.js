e40c463e37ff645c5bad5afca170cf8aed50fca8
Bill Mania <bill@manialabs.us>
2024-09-07 10:39:56 -0500

    Added a blank option to the widget type menu for configuration, to detect every widget type change. When the widget type is blank, the format section of the input form is hidden. Added the utility function rosConfiguration which is called each time the widget type is changed. Added the constant WIDGET_TYPES to validate the type selection. Added the constant widgetInterface to provide the configuration options for the non-gamepad widget types. This constant is provides information similar to src/ros_interfaces.js, but much simpler. Corrected two bugs in widget_config.js related to undefined widgetType.
    https://github.com/billmania/roboquest_ui/issues/149

--- public/js/widget_config.js
+++ public/js/widget_config.js
@@ -10,6 +10,15 @@
 */
 let socket
 
+const WIDGET_TYPES = [
+  'button',
+  'gamepad',
+  'indicator',
+  'joystick',
+  'slider',
+  'value'
+]
+
 /*
  * So the widget default configuration can be shown for a new
  * widget but not for an existing widget.
@@ -98,7 +107,7 @@ const updateWidgetPosition = function (oldPosition, newPosition) {
  *
  * The structure of a new widget is comprised of three parts:
  * 1. the top level, all-encompassing element is a widgetContainer and is
- *    and HTML DIV with the CSS class "widget" and widgetType. it also
+ *    an HTML DIV with the CSS class "widget" and widgetType. it also
  *    has an HTML ID unique to this widget
  * 2. the widgetContainer includes a widgetHeader which shows the
  *    widget's label
@@ -351,10 +360,14 @@ const reconfigureWidget = function (oldWidgetConfig, newWidgetConfig) {
  * The global variable showConfigDefaults prevents over-writing an
  * existing configuration with the defaults.
  */
+// TODO: Constrain configuration using widgetInterface based on widgetType
 const setWidgetConfigDefaults = function () {
   if (showConfigDefaults) {
     const widgetType = jQuery('#newWidget #newWidgetType').find('option:selected').val()
-    populateWidgetConfigurationDialog(widgetDefaults[widgetType])
+    if (WIDGET_TYPES.includes(widgetType)) {
+      console.debug(`setWidgetConfigDefaults: ${JSON.stringify(widgetDefaults[widgetType])}`)
+      populateWidgetConfigurationDialog(widgetDefaults[widgetType])
+    }
     return
   }
   showConfigDefaults = true
@@ -370,25 +383,39 @@ const setWidgetConfigDefaults = function () {
  */
 const setNewWidgetDialogType = function (widgetType) {
   jQuery('#newWidget .newWidgetType').hide()
-  jQuery(`#newWidget #${widgetType}`).show()
-  if (widgetType === 'gamepad') {
-    if (gamepad.gamepadConnected()) {
-      gamepad.enableGamepad()
-    } else {
-      showMsg('No gamepad connected')
-      console.warn('setNewWidgetDialogType: No gamepad connected')
+  if (WIDGET_TYPES.includes(widgetType)) {
+    jQuery(`#newWidget #${widgetType}`).show()
+    if (widgetType === 'gamepad') {
+      if (gamepad.gamepadConnected()) {
+        gamepad.enableGamepad()
+      } else {
+        showMsg('No gamepad connected')
+        console.warn('setNewWidgetDialogType: No gamepad connected')
+      }
     }
   }
 }
 
 /**
- * Used when re-configuring an existing widget.
+ * Used when adding a new widget and re-configuring an existing widget.
  *
- * @param {object} widgetConfig - the current configuration to use as
- *                                default values.
+ * @param {object} widgetConfig - the current configuration when
+ *                                reconfiguring OR the default
+ *                                values when adding a new widget
+ *
+ * If widgetConfig.type === undefined, this is a new widget,
+ * otherwise it's an existing widget being reconfigured.
+ *
  */
 const populateWidgetConfigurationDialog = function (widgetConfig) {
+  if (widgetConfig === undefined) {
+    return
+  }
+
   if (widgetConfig.type === 'gamepad') {
+    /*
+     * Re-configuring an existing gamepad widget.
+     */
     gamepad.parseConfig(widgetConfig)
   }
 
@@ -597,6 +624,13 @@ const extractWidgetConfigurationFromDialog = function () {
   return objNewWidget
 }
 
+/**
+ * Manage the ROS configuration of a widget.
+ */
+const rosConfiguration = function (genericArg) { // eslint-disable-line no-unused-vars
+  console.debug(`rosConfiguration: ${JSON.stringify(genericArg)}`)
+}
+
 const initWidgetConfig = function (objSocket) { // eslint-disable-line no-unused-vars
   socket = objSocket
   /**
@@ -644,9 +678,14 @@ const initWidgetConfig = function (objSocket) { // eslint-disable-line no-unused
     }
   })
 
+  /**
+   * Capture changes to the widget configuration type.
+   */
   jQuery('#newWidget #newWidgetType').selectmenu({
     change: function (event, ui) {
       const widgetType = ui.item.value
+
+      rosConfiguration(widgetType)
 
       setWidgetConfigDefaults()
       setNewWidgetDialogType(widgetType)
@@ -655,7 +694,7 @@ const initWidgetConfig = function (objSocket) { // eslint-disable-line no-unused
 
   jQuery('#addWidget').on('click', function () {
     /*
-     * The #newWidget dialog is used to define a new widget, which
+     * The #newWidget dialog is used to define a new widget which
      * doesn't yet exist in the widget configuration file.
      */
     jQuery('#newWidget').dialog({
@@ -667,10 +706,10 @@ const initWidgetConfig = function (objSocket) { // eslint-disable-line no-unused
         }
       },
       open: function (event, ui) {
+        setNewWidgetDialogType('')
         keyControl.disableKeys()
         setWidgetConfigDefaults()
         configuringWidget = true
-        console.debug('configuringWidget is true')
       },
       close: function (event, ui) {
         gamepad.disableGamepad()
@@ -762,5 +801,128 @@ const widgetDefaults = {
     label: 'gamepadX',
     format: {},
     data: {}
+  }
+}
+
+/*
+ * The widget_interface object is used similarly to the objects in src/ros_interfaces.js.
+ * It defines the allowed service, topic, and attributes allowed for each widget
+ * type.
+ * Widgets are more constrained than the gamepad. There aren't any widgets which have
+ * the option of either a topic or a service.
+ */
+const widgetInterface = {
+  button: {
+    service: {
+      control_hat: {
+        'rq_msgs/srv/Control': [
+          'set_motors:ON',
+          'set_servos:ON',
+          'set_fet1:ON',
+          'set_fet2:ON',
+          'set_charger:ON'
+        ]
+      }
+    }
+  },
+
+  slider: {
+    topic: {
+      publish: {
+        motor_speed: {
+          'rq_msgs/msg/MotorSpeed': [
+            'max_rpm'
+          ]
+        },
+
+        servos: {
+          'rq_msgs/msg/Servos': [
+            'servo0.angle_deg;servo0.command_type:1',
+            'servo1.angle_deg;servo1.command_type:1',
+            'servo2.angle_deg;servo2.command_type:1',
+            'servo3.angle_deg;servo3.command_type:1',
+            'servo4.angle_deg;servo4.command_type:1',
+            'servo5.angle_deg;servo5.command_type:1',
+            'servo6.angle_deg;servo6.command_type:1',
+            'servo7.angle_deg;servo7.command_type:1',
+            'servo8.angle_deg;servo8.command_type:1',
+            'servo9.angle_deg;servo9.command_type:1',
+            'servo10.angle_deg;servo10.command_type:1',
+            'servo11.angle_deg;servo11.command_type:1',
+            'servo12.angle_deg;servo12.command_type:1',
+            'servo13.angle_deg;servo13.command_type:1',
+            'servo14.angle_deg;servo14.command_type:1',
+            'servo15.angle_deg;servo15.command_type:1'
+          ]
+        }
+      }
+    }
+  },
+
+  value: {
+    topic: {
+      subscribe: {
+        telemetry: {
+          'rq_msgs/msg/Telemetry': [
+            'battery_v',
+            'battery_ma',
+            'system_ma',
+            'adc0_v',
+            'adc1_v',
+            'adc2_v',
+            'adc3_v',
+            'adc4_v'
+          ]
+        }
+      }
+    }
+  },
+
+  indicator: {
+    topic: {
+      subscribe: {
+        telemetry: {
+          'rq_msgs/msg/Telemetry': [
+            'charger_has_power',
+            'battery_charging',
+            'motors_on',
+            'servos_on'
+          ]
+        }
+      }
+    }
+  },
+
+  joystick: {
+    topic: {
+      publish: {
+        servos: {
+          'rq_msgs/msg/Servos': [
+            'servo0.speed_dps;servo0.command_type:3',
+            'servo1.speed_dps;servo1.command_type:3',
+            'servo2.speed_dps;servo2.command_type:3',
+            'servo3.speed_dps;servo3.command_type:3',
+            'servo4.speed_dps;servo4.command_type:3',
+            'servo5.speed_dps;servo5.command_type:3',
+            'servo6.speed_dps;servo6.command_type:3',
+            'servo7.speed_dps;servo7.command_type:3',
+            'servo8.speed_dps;servo8.command_type:3',
+            'servo9.speed_dps;servo9.command_type:3',
+            'servo10.speed_dps;servo10.command_type:3',
+            'servo11.speed_dps;servo11.command_type:3',
+            'servo12.speed_dps;servo12.command_type:3',
+            'servo13.speed_dps;servo13.command_type:3',
+            'servo14.speed_dps;servo14.command_type:3',
+            'servo15.speed_dps;servo15.command_type:3'
+          ]
+        },
+
+        cmd_vel: {
+          'geometry_msgs/msg/TwistStamped': [
+            'twist.angular.z;twist.linear.x'
+          ]
+        }
+      }
+    }
   }
 }
